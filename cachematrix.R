377c1122abb875294bf7e8fdd3933899c0948b44
mallku <johnybray@hotmail.com>
2014-11-22 12:10:33 +0000

    adding revised cachematrix.R script for programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Lexical scoping, takes a matrix object and returns the inverse of this
+## caches the result in memory so as to not need to compute the inverse each time on the same matrix if unchanged
+# please note: the full code below needs to be run for this to work and the matrix to test
+# is initialised at the bottom called variable b.
 
-## Write a short comment describing this function
+a <- matrix(c(1,2,3,4), nrow=2, ncol=2) # create a matrix to put into the function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+    
 }
 
-
-## Write a short comment describing this function
+## temporarily stores the solution as a variable and returns this if it has already been calculated otherwise solves the
+# inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+    
 }
+
+b <- makeCacheMatrix(a) # initialise the test matrix
